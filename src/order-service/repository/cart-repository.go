be934e4bb8747787b9c781a57f6558bb2901e088
Duy Vo <vodongducduyqn97@gmail.com>
2022-07-17 02:01:40 +0700

    Add Api GetCart and GetListCart

--- src/order-service/repository/cart-repository.go
+++ src/order-service/repository/cart-repository.go
@@ -4,11 +4,12 @@ import (
 	"chilindo/src/order-service/dtos"
 	"chilindo/src/order-service/models"
 	"gorm.io/gorm"
+	"log"
 )
 
 type ICartRepository interface {
 	GetCart(dto *dtos.CartDTO) (*models.Cart, error)
-	GetListCarts(dto *dtos.UserCartDTO) (*models.Cart, error)
+	GetListCarts(dto *dtos.UserCartDTO) (*[]models.Cart, error)
 }
 
 type CartRepository struct {
@@ -16,13 +17,23 @@ type CartRepository struct {
 }
 
 func (c CartRepository) GetCart(dto *dtos.CartDTO) (*models.Cart, error) {
-	//TODO implement me
-	panic("implement me")
+	var cart *models.Cart
+	record := c.db.Where("cart_id = ? ", dto.CartId).Find(&cart)
+	if record.Error != nil {
+		log.Println("GetCart: Error to query", record.Error)
+		return nil, record.Error
+	}
+	return cart, nil
 }
 
-func (c CartRepository) GetListCarts(dto *dtos.UserCartDTO) (*models.Cart, error) {
-	//TODO implement me
-	panic("implement me")
+func (c CartRepository) GetListCarts(dto *dtos.UserCartDTO) (*[]models.Cart, error) {
+	var cart *[]models.Cart
+	record := c.db.Where("user_id = ? ", dto.UserId).Find(&cart)
+	if record.Error != nil {
+		log.Println("GetCart: Error to query", record.Error)
+		return nil, record.Error
+	}
+	return cart, nil
 }
 
 func NewCartRepository(db *gorm.DB) *CartRepository {
